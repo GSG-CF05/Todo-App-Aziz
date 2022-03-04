67b11016423682aafa566e78b2fd8f05ab1c91a7
azzsal <azzsal@protonmail.com>
2022-03-04 16:53:36 +0200

    create overlay section that is used for mobile #relates #15

--- js/toggle-navbar.js
+++ js/toggle-navbar.js
@@ -1,9 +1,22 @@
 (function() {
   const menuBtn = document.querySelector(".menu");
   const navbar = document.querySelector(".navbar");
+  const pageContainer = document.querySelector(".page-container");
+  const logoLink = document.querySelector(".header .logo .go-home");
+  const pageOverlay = document.querySelector(".page-container div");
 
   menuBtn.addEventListener("click", () => {
     navbar.classList.toggle('show-menu');
+    logoLink.classList.toggle("disabled");
+    pageOverlay.classList.toggle("page-overlay");
   });
+  
+  pageContainer.addEventListener("click", (e) => {
+    if(e.currentTarget == pageContainer && e.target != document.querySelector(".fa-solid")) {
+      navbar.classList.remove('show-menu');
+      logoLink.classList.remove("disabled");
+      pageOverlay.classList.remove("page-overlay");
+    }
+  })
 }());
 
